d6bf32d6bbea48e53e5c48f8191534893b197be4
Wassim KABALAN <wastondev@gmail.com>
2024-10-02 10:02:14 +0200

    update implementation

--- src/csrc/butterfly_fft.cu
+++ src/csrc/butterfly_fft.cu
@@ -1,125 +1,131 @@
-#include "collectives/butterfly_comm_index.hpp"
-#include "collectives/collective_ops.hpp"
-#include "common/ffi_helper.hpp"
 #include "extensions.h"
+#include <cute/tensor.hpp>
+// #include <cute/layout.hpp>
+// #include <cute/stride.hpp>
+#include "cute/algorithm/tensor_algorithms.hpp"
+#include "cute/stride.hpp"
+#include "cute/tensor_impl.hpp"
+#include "arithmetics.cuh"
+#include "collectives/butterfly_comm_index.hpp"
+#include "common/ffi_helper.hpp"
 #include "xla/ffi/api/api.h"
 #include "xla/ffi/api/ffi.h"
-#include <cstddef>
-#include <cstdint>
-#include <cuda_runtime.h>
 #include <nccl.h>
+#include "collectives/nccl_ops.hpp"
 
 namespace ffi = xla::ffi;
-
-template <typename T> __global__ void AddElementKernel(T *x, T *y, int n) {
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  if (i < n) {
-    y[i] = x[i] * 2;
-  }
-}
+using namespace cute;
 
 template <DataType dtype>
-ffi::Error ButterFlyForward(cudaStream_t stream, Buffer<dtype> x, NORM iNorm,
-                            Result<dtype> y, CommIterator &comms) {
-  CCO::CollectiveOps ops;
-  CCO::ReductionOp reduc_op{CCO::ReducType::SUM};
-  auto stageComm = comms.next();
-  assertm(stageComm.has_value(),
-          "[INTERNAL] Distributed FFT called without Buttefly comms");
+ffi::Error ButterFlyForward(cudaStream_t stream, Buffer<dtype> x, NORM iNorm, Result<dtype> y, CommIterator &comms) {
+    const int &device_rank = CCO::NCCLOps::get_rank();
+    const int &device_count = CCO::NCCLOps::get_size();
+    const int global_axis_size = x.dimensions()[0] * device_count;
+    // Get the cute Tensor from the XLA Buffer
+    Dimensions dims = x.dimensions();
+    assertm(dims.size() == 3, "Only 3D tensors are supported for now");
+    // Create a static rank 3 tensor from the buffer
+    auto tensor_shape = make_shape(dims[0], dims[1], dims[2]);
+    Tensor tensor_S = make_tensor(make_gmem_ptr(x.typed_data()), make_layout(tensor_shape));
+    Tensor tensor_D = make_tensor(make_gmem_ptr(y->typed_data()), make_layout(tensor_shape));
+    using TensorType = typename decltype(tensor_S)::value_type;
+    // Create the kernel configuration and the tiled tensor
+    auto block_shape = Shape<_64, _4, _4>{};
+    Layout thread_layout = make_layout(Shape<_64, _4, _4>{});  // 256 threads one thread per element
+    Tensor tiled_tensor_S = tiled_divide(tensor_S, block_shape);
+    Tensor tiled_tensor_D = tiled_divide(tensor_D, block_shape);
+    // Grid dim is the result of the division of the tensor shape by the block Shape
+    // Thread layout is the shape of the block to make sure that each thread is responsible for one element
+    dim3 gridDim(size<1>(tiled_tensor_S), size<2>(tiled_tensor_S), size<3>(tiled_tensor_S));
+    dim3 blockDim(shape<0>(thread_layout), shape<1>(thread_layout), shape<2>(thread_layout));
+    // Get the first stage comm .. this is called in a distributed setup and the first step
+    // Means that we have atleast 2 devices in the comm .. otherwise this is an error
 
-  // Get the Stage comm and the N (normalization factor for this stage)
-  auto [N, comm] = stageComm.value();
-  int device_count;
-  NCCLCHECK(ncclCommCount(comm, &device_count));
-  assertm(device_count == 2, "In each step the communication is pairwise");
-
-  // First step A + B
-  ops.reduce(x.typed_data(), y->typed_data(), x.element_count(), reduc_op, 0,
-             comm, stream);
-  // Second step A - B
-  ops.reduce(y->typed_data(), y->typed_data(), y->element_count(), reduc_op, 1,
-             comm, stream);
-
-  // apply the twiddle factor
-
-  // Continue with the rest of the stages
-  while ((stageComm = comms.next())) {
+    auto stageComm = comms.next();
+    assertm(stageComm.has_value(), "[INTERNAL] Distributed FFT called without Buttefly comms");
+    // Get the Stage comm and the N (normalization factor for this stage)
     auto [N, comm] = stageComm.value();
-    ops.reduce(y->typed_data(), y->typed_data(), y->element_count(), reduc_op,
-               0, comm, stream);
-    ops.reduce(y->typed_data(), y->typed_data(), y->element_count(), reduc_op,
-               1, comm, stream);
-  }
-  return ffi_with_cuda_error_check();
+    int stage_rank;
+    NCCLCHECK(ncclCommUserRank(comm, &stage_rank));
+    // First step A + B
+    ncclGroupStart();
+    ncclReduce(x.untyped_data(), y->untyped_data(), x.element_count(), ncclFloat, ncclSum, 0, comm, stream);
+
+    // Second step A - B
+    // First add -2 to B to prepare for => -2B + (A + B) = A - B
+    if (stage_rank == 1) {
+        Multiply<<<gridDim, blockDim, 0, stream>>>(tiled_tensor_S, tiled_tensor_D, (TensorType)-2, thread_layout);
+    }
+    // ncclReduce(y->untyped_data(), y->untyped_data(), x.element_count(), ncclFloat, ncclSum, 1, comm, stream);
+    std::cout << "Reduced " << std::endl;
+    // Apply the twiddle factor
+    // Check if current device holds the A or B
+    if (stage_rank == 1) {
+        // ApplyTwiddle<<<gridDim, blockDim, 0, stream>>>(tiled_tensor_S, global_axis_size, N, device_rank, device_count, thread_layout);
+    }
+
+    // Continue with the rest of the stages
+    while ((stageComm = comms.next())) {
+        auto [N, comm] = stageComm.value();
+
+        int stage_rank;
+        int stage_count;
+
+        NCCLCHECK(ncclCommUserRank(comm, &stage_rank));
+        NCCLCHECK(ncclCommCount(comm, &stage_count));
+
+        std::cout << "For the next stage N " << N << " stage rank " << stage_rank << " stage count " << stage_count << std::endl;
+    }
+    ncclGroupEnd();
+
+    return ffi_with_cuda_error_check();
 }
 
 template <DataType dtype>
-ffi::Error ButterFlyBackward(cudaStream_t stream, Buffer<DataType::F32> x,
-                             NORM iNorm, Result<dtype> y, CommIterator &comms) {
+ffi::Error ButterFlyBackward(cudaStream_t stream, Buffer<DataType::F32> x, NORM iNorm, Result<dtype> y, CommIterator &comms) {
+    comms.reset();
+    auto comm = comms.prev();
+    assertm(comm.has_value(), "[INTERNAL] Distributed IFFT called without Buttefly comms");
 
-  CCO::CollectiveOps ops;
-  CCO::ReductionOp reduc_op{CCO::ReducType::SUM};
-  comms.reset();
-  auto comm = comms.prev();
-  assertm(comm.has_value(),
-          "[INTERNAL] Distributed IFFT called without Buttefly comms");
-
-  // ops.allreduce(x.typed_data(), y->typed_data(), x.element_count(), reduc_op,
-  //               comm.value(), stream);
-  //
-  // while ((comm = comms.prev())) {
-  //   ops.allreduce(y->typed_data(), y->typed_data(), x.element_count(),
-  //   reduc_op,
-  //                 comm.value(), stream);
-  // }
-  return ffi_with_cuda_error_check();
+    // ops.allreduce(x.typed_data(), y->typed_data(), x.element_count(), reduc_op,
+    //               comm.value(), stream);
+    //
+    // while ((comm = comms.prev())) {
+    //   ops.allreduce(y->typed_data(), y->typed_data(), x.element_count(),
+    //   reduc_op,
+    //                 comm.value(), stream);
+    // }
+    return ffi_with_cuda_error_check();
 }
 
 template <DataType dtype>
-ffi::Error ButterFlyFFT(cudaStream_t stream, Buffer<DataType::F32> x,
-                        int64_t iDirection, int64_t iAxis, int64_t iNorm,
-                        Result<dtype> y) {
+ffi::Error ButterFlyFFT(cudaStream_t stream, Buffer<DataType::F32> x, int64_t iDirection, int64_t iAxis, int64_t iNorm, Result<dtype> y) {
+    AXIS axis = static_cast<AXIS>(iAxis);
+    DIRECTION direction = static_cast<DIRECTION>(iDirection);
+    NORM norm = static_cast<NORM>(iNorm);
 
-  AXIS axis = static_cast<AXIS>(iAxis);
-  DIRECTION direction = static_cast<DIRECTION>(iDirection);
-  NORM norm = static_cast<NORM>(iNorm);
+    assertm(axis == AXIS::X, "Only X axis is supported");
 
-  assertm(axis == AXIS::X, "Only X axis is supported");
+    ncclComm_t comm = CCO::NCCLOps::get_comm();
 
-  ncclComm_t comm = CCO::NCCLOps::get_comm();
-  const int &rank = CCO::NCCLOps::get_rank();
-  const int &size = CCO::NCCLOps::get_size();
+    auto butterfly_comm = ButterflyCommIndex::get_or_create_comms(comm);
 
-  auto butterfly_comm = ButterflyCommIndex::get_or_create_comms(comm);
-  CCO::ReductionOp reduc_op{CCO::ReducType::SUM};
-
-  switch (direction) {
-  case DIRECTION::FORWARD:
-    return ButterFlyForward(stream, x, norm, y, butterfly_comm);
-  case DIRECTION::INVERSE:
-    return ButterFlyBackward(stream, x, norm, y, butterfly_comm);
-  default:
-    return ffi::Error(XLA_FFI_Error_Code_INTERNAL,
-                      std::string("Un recongnized FFT direction "));
-  }
+    switch (direction) {
+        case DIRECTION::FORWARD:
+            return ButterFlyForward(stream, x, norm, y, butterfly_comm);
+        case DIRECTION::INVERSE:
+            return ButterFlyBackward(stream, x, norm, y, butterfly_comm);
+        default:
+            return ffi::Error(XLA_FFI_Error_Code_INTERNAL, std::string("Un recongnized FFT direction "));
+    }
 }
 
-template <DataType dtype>
-ffi::Error TestFunc(cudaStream_t stream, Buffer<dtype> x, Result<dtype> y) {
-  int blockSize = 256;
-  int numBlocks = (x.element_count() + blockSize - 1) / blockSize;
-  AddElementKernel<<<numBlocks, blockSize, 0, stream>>>(
-      x.typed_data(), y->typed_data(), x.element_count());
-  return ffi_with_cuda_error_check();
-}
-
-XLA_FFI_DEFINE_HANDLER_SYMBOL(ButterFlyFFTHandlerF32,
-                              ButterFlyFFT<DataType::F32>,
+XLA_FFI_DEFINE_HANDLER_SYMBOL(ButterFlyFFTHandlerF32, ButterFlyFFT<DataType::F32>,
                               ffi::Ffi::Bind()
-                                  .Ctx<FFI_Stream_Type>()
-                                  .Arg<Buffer<DataType::F32>>() // x
-                                  .Attr<int64_t>("direction")
-                                  .Attr<int64_t>("norm")
-                                  .Attr<int64_t>("axis")
-                                  .Ret<Buffer<DataType::F32>>() // y
+                                      .Ctx<FFI_Stream_Type>()
+                                      .Arg<Buffer<DataType::F32>>()  // x
+                                      .Attr<int64_t>("direction")
+                                      .Attr<int64_t>("norm")
+                                      .Attr<int64_t>("axis")
+                                      .Ret<Buffer<DataType::F32>>()  // y
 );
--- src/csrc/collectives.cc
+++ src/csrc/collectives.cc
@@ -1,47 +1,52 @@
 
-#include "collectives/collective_ops.hpp"
 #include "extensions.h"
 #include "xla/ffi/api/api.h"
 #include "xla/ffi/api/ffi.h"
 #include <cuda_runtime.h>
-
+#include "collectives/nccl_ops.hpp"
+#include "collectives/mpi_ops.hpp"
 
 namespace ffi = xla::ffi;
 
-ffi::Error AllReduceNCCLImpl(cudaStream_t stream, ffi::Buffer<ffi::DataType::F32> x,
-                       ffi::Result<ffi::Buffer<ffi::DataType::F32>> y) {
+ffi::Error AllReduceNCCLImpl(cudaStream_t stream, ffi::Buffer<ffi::DataType::F32> x, ffi::Result<ffi::Buffer<ffi::DataType::F32>> y) {
+    ncclComm_t comm = CCO::NCCLOps::get_comm();
+    int rank = CCO::NCCLOps::get_rank();
 
-  CCO::CollectiveOps ops;
-  CCO::ReductionOp op(CCO::ReducType::SUM);
-  ncclComm_t comm = CCO::NCCLOps::get_comm();
-  ops.allreduce(x.typed_data() , y->typed_data(), x.element_count(), op, comm , stream);
+    int color = rank % 2;
+    ncclComm_t split_comm;
+    ncclCommSplit(comm, color, rank, &split_comm, nullptr);
 
-  return ffi::Error::Success();
+    int local_rank;
+    int local_count;
+
+    ncclCommUserRank(split_comm, &local_rank);
+    ncclCommCount(split_comm, &local_count);
+
+    std::cout << "NCCL Rank: " << rank << " NCCL Local Rank: " << local_rank << " NCCL Local Count: " << local_count << std::endl;
+
+    ncclAllReduce(x.untyped_data(), y->untyped_data(), x.element_count(), ncclFloat, ncclSum, split_comm, stream);
+
+    return ffi::Error::Success();
 }
 
-ffi::Error AllReduceMPIImpl(cudaStream_t stream,ffi::Buffer<ffi::DataType::F32> x,
-                       ffi::Result<ffi::Buffer<ffi::DataType::F32>> y) {
+ffi::Error AllReduceMPIImpl(cudaStream_t stream, ffi::Buffer<ffi::DataType::F32> x, ffi::Result<ffi::Buffer<ffi::DataType::F32>> y) {
+    MPI_Comm mpi_comm = CCO::MPIOps::get_comm();
+    // ops.allreduce(x.typed_data(), y->typed_data(), x.element_count(), op,
+    //               mpi_comm);
 
-  CCO::CollectiveOps ops;
-  CCO::ReductionOp op(CCO::ReducType::SUM);
-  MPI_Comm mpi_comm = CCO::MPIOps::get_comm();
-  ops.allreduce(x.typed_data() , y->typed_data(), x.element_count(), op, mpi_comm);
-
-  return ffi::Error::Success();
+    return ffi::Error::Success();
 }
 
 XLA_FFI_DEFINE_HANDLER_SYMBOL(AllReduceNCCLF32, AllReduceNCCLImpl,
                               ffi::Ffi::Bind()
-                                  .Ctx<ffi::PlatformStream<cudaStream_t>>()
-                                  .Arg<ffi::Buffer<ffi::DataType::F32>>() // x
-                                  .Ret<ffi::Buffer<ffi::DataType::F32>>() // y
+                                      .Ctx<ffi::PlatformStream<cudaStream_t>>()
+                                      .Arg<ffi::Buffer<ffi::DataType::F32>>()  // x
+                                      .Ret<ffi::Buffer<ffi::DataType::F32>>()  // y
 );
 
 XLA_FFI_DEFINE_HANDLER_SYMBOL(AllReduceMPIF32, AllReduceMPIImpl,
                               ffi::Ffi::Bind()
-                                  .Ctx<ffi::PlatformStream<cudaStream_t>>()
-                                  .Arg<ffi::Buffer<ffi::DataType::F32>>() // x
-                                  .Ret<ffi::Buffer<ffi::DataType::F32>>() // y
+                                      .Ctx<ffi::PlatformStream<cudaStream_t>>()
+                                      .Arg<ffi::Buffer<ffi::DataType::F32>>()  // x
+                                      .Ret<ffi::Buffer<ffi::DataType::F32>>()  // y
 );
-
-
